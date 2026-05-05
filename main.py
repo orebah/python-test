02889cdc7353ec94e89bc28c9ad189935ba7b810
orebah <orebah@wp.pl>
2026-05-05 19:47:55 +0200

    Cleanup and prepare test branch

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
 import sys
 print(f"Hello! Skrypt Python uruchomiony pomyślnie hrhshshfshsdhs.")
 print(f"Wersja interpretera: {sys.version}")
-print(f"Hello! Skrypt Python uruchomiony pomyślnie hrhshshfshsdhs.")
